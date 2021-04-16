0019bafca67a4ad004867b2d9115413a4bc4a01c
Aditya203204 <76596474+Aditya203204@users.noreply.github.com>
2021-04-16 17:46:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas=new frabic.Canvas("myCanvas");
+var canvas=new fabric.Canvas("myCanvas");
 block_image_width=30;
 block_image_height=30;
 player_x=10;
@@ -6,7 +6,7 @@ player_y=10;
 var player_object="";
 function player_update()
 {
-    fabic.Image.fromURL("player.png",function(Img){
+    fabric.Image.fromURL("player.png",function(Img){
         player_object=Img;
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
@@ -18,7 +18,7 @@ canvas.add(player_object);
 }
 function new_image(get_image)
 {
-    fabic.Image.fromURL(get_image,function(Img){
+    fabric.Image.fromURL(get_image,function(Img){
         block_image_object=Img;
         block_image_object.scaleToWidth(block_image_width);
         block_image_object.scaleToHeight(block_image_height);
